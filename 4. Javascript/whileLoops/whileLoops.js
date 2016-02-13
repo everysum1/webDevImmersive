6010dd69e97b40a919a78446758c707cdd18bbed
Israel Matos <israeldmatos@gmail.com>
2016-02-13 03:14:14 -0800

    adding solutions

---
+++ 4. Javascript/whileLoops/whileLoops.js
@@ -0,0 +1,28 @@
+
+var count = -10
+while (count < 20) {
+	console.log(count)
+	count++
+}
+
+var even = 10
+
+while (even < 41) {
+	console.log(even)
+	even+=2
+}
+
+var odd = 301
+
+while (odd < 334) {
+	console.log(odd)
+	odd+=2
+}
+
+var i = 5
+while (i < 51) {
+	if (i % 3 === 0 && i % 5 === 0) {
+		console.log(i)
+	}
+	i++
+}
