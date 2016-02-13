bdc3878eb97232edfd74151a66e11cee35350198
Israel Matos <israeldmatos@gmail.com>
2016-02-13 03:13:06 -0800

    Functionality updates to ToDo List app

--- 4. Javascript/toDoList/todo.js
+++ 4. Javascript/toDoList/todo.js
@@ -3,15 +3,36 @@ var todos = [];
 var answer = prompt("What would you like to do?")
 
 while (answer !== "quit") {
-	if (answer === "new") {
-		var newTask = prompt("What would you like to add to the list?");
-		todos.push(newTask);
 
+	if (answer === "new") {
+		addToDo();
 	} else if (answer === "list") {
-		console.log(todos);
-	} 
+		listToDos();
+	} else if (answer === "delete") {
+		deleteToDo();
+	}
 
 	answer = prompt("What would you like to do?")
 }
 
 console.log("You have quit the app");
+
+function addToDo() {
+	var newTask = prompt("Enter new to do:");
+	todos.push(newTask);
+	console.log("Item Added")
+}
+
+function listToDos() {
+	console.log("**********************");
+	todos.forEach(function(todo, i) {
+		console.log((i+1) + ": " + todo);
+	})
+	console.log("**********************");
+}
+
+function deleteToDo() {
+	var index = prompt("Enter number of the item to remove:")
+	todos.splice(index-1, 1);
+	console.log("Item Deleted")
+}
