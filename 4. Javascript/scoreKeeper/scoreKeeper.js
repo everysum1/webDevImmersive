94d5d5f029e3a513f680eacb79a05d2f266012f4
Israel Matos <israeldmatos@gmail.com>
2016-02-14 03:29:00 -0800

    functionality & interactivity

---
+++ 4. Javascript/scoreKeeper/scoreKeeper.js
@@ -0,0 +1,54 @@
+var p1button = document.querySelector("#p1");
+var p2button = document.querySelector("#p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display")
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
+var resetButton = document.querySelector("#reset");
+
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+p1button.addEventListener("click", function() {
+	if (!gameOver) {
+		p1Score++;
+		if (p1Score === winningScore) {
+			gameOver = true;
+			p1Display.classList.add("winner")
+		}
+		p1Display.textContent = p1Score;
+	}
+});
+
+p2button.addEventListener("click", function() {
+	if (!gameOver) {
+		p2Score++;
+		if (p2Score === winningScore) {
+			gameOver = true;
+			p2Display.classList.add("winner")
+		}
+		p2Display.textContent = p2Score;
+	}
+});
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
+});
+
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset() {
+	gameOver = false;
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+}
